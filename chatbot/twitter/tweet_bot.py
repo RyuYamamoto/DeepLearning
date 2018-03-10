f2b7af30f519921450c191ee9ae4463c2ffd313e
RyuYamamoto <s1226120za@s.chibakoudai.jp>
2018-03-10 15:11:14 +0900

    get image file from web camera

--- chatbot/twitter/tweet_bot.py
+++ chatbot/twitter/tweet_bot.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
+import numpy as np
+import cv2
 import re
+import os
 import json
 import requests 
 import tweepy
@@ -21,22 +24,33 @@ auth.set_access_token(settings.ACCESS_TOKEN, settings.ACCESS_TOKEN_SECRET)
  
 api = tweepy.API(auth) 
 
+capture = cv2.VideoCapture(0)
+
 class Listener(tweepy.StreamListener):
     def on_status(self, status):
         status.created_at += datetime.timedelta(hours=9)
 
         # リプライが来たら返信
         if str(status.in_reply_to_screen_name)=="bot_api_ai":# and str(status.user.screen_name)=="{Twitter_ID}":
-            payload['utt'] = re.sub("@bot_api_ai ", "",status.text)
-            payload['nickname'] = status.user.screen_name
-            url = 'https://api.apigw.smt.docomo.ne.jp/dialogue/v1/dialogue?APIKEY='+settings.API_KEY
-            r = requests.post(url, data=json.dumps(payload))
-            res_json = r.json()
-            payload['context'] = res_json['context']
+            if str(status.text) == u"@bot_api_ai photo":
+                print "OK"
+                ret, frame = capture.read()
+                path = "photo.png"
+                cv2.imwrite(path,frame)
+                tweet = "@" + str(status.user.screen_name) + "\n"
+                api.update_with_media(filename="photo.png", status=tweet)
+                os.remove(path)
+            else:
+                payload['utt'] = re.sub("@bot_api_ai ", "",status.text)
+                payload['nickname'] = status.user.screen_name
+                url = 'https://api.apigw.smt.docomo.ne.jp/dialogue/v1/dialogue?APIKEY='+settings.API_KEY
+                r = requests.post(url, data=json.dumps(payload))
+                res_json = r.json()
+                payload['context'] = res_json['context']
 
-            status_id=status.id
-            tweet = "@" + str(status.user.screen_name) + " " + res_json['utt'].encode('utf-8') + "\n"
-            api.update_status(status=tweet, in_reply_to_status_id=status_id)
+                status_id=status.id
+                tweet = "@" + str(status.user.screen_name) + " " + res_json['utt'].encode('utf-8') + "\n"
+                api.update_status(status=tweet, in_reply_to_status_id=status_id)
         return True
 
     def on_error(self, status_code):
@@ -50,3 +64,6 @@ class Listener(tweepy.StreamListener):
 listener = Listener()
 stream = tweepy.Stream(auth, listener)
 stream.userstream()
+
+capture.release()
+cv2.destroyAllWindows()
